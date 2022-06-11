cf763def5b7b5e0dfff5db34b11fad01b2275d50
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-11 08:49:28 +0300

    commit tree cd3e8d49d14f5f363e317e0ec15e5c265d1c7f0c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const elementMain = document.getElementById("main");
+elementMain.remove();
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "YOUR-NAME is the champion";
+document.body.append(newHeader);
